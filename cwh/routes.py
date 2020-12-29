227c5fc58672316b8a5d85ad485176302d04dd24
Mihir Sharma <65905351+mihir910@users.noreply.github.com>
2020-12-29 20:43:28 +0530

    Add files via upload

---
+++ cwh/routes.py
@@ -0,0 +1,59 @@
+from flask import render_template, url_for, flash, redirect
+from cwh import app
+from cwh.forms import RegistrationForm, LoginForm
+# from cwh.models import User, Post
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html',title="Home")
+
+
+@app.route("/videos")
+def videos():
+    return render_template('videos.html', title='Videos')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title='Contact')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html', title='Blog')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    # return render_template( title='Login', form=form)
